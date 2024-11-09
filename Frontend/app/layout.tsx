6b5aac52c27acfc6e7e75692c51502f39c4541a7
jhoiner <jhoinermartinez243@gmail.com>
2024-11-08 23:50:32 -0500

    Layout con header

--- Frontend/app/layout.tsx
+++ Frontend/app/layout.tsx
@@ -1,18 +1,53 @@
-import { SidebarProvider, SidebarTrigger } from "../src/components/ui/sidebar"
-import { AppSidebar } from "../src/components/ui/app-sidebar"
 import {Outlet} from 'react-router-dom'
 import { ThemeProvider } from "../src/components/ui/proveedor-temas"
 
-export default function Layout() {
+import { AppSidebar } from "@/components/ui/Sidebar/app-sidebar"
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb"
+import { Separator } from "@/components/ui/separator"
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar"
+
+export default function Layout()  {
   return (
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+    <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
     <SidebarProvider>
       <AppSidebar />
-      <main className="w-full">
-        <SidebarTrigger />
-        <Outlet />
-      </main>
+      <SidebarInset>
+        <header className="flex h-16 shrink-0 items-center gap-2">
+          <div className="flex items-center gap-2 px-4">
+            <SidebarTrigger className="-ml-1" />
+            <Separator orientation="vertical" className="mr-2 h-4" />
+            <Breadcrumb>
+              <BreadcrumbList>
+                <BreadcrumbItem className="hidden md:block">
+                  <BreadcrumbLink href="#">
+                    Building Your Application
+                  </BreadcrumbLink>
+                </BreadcrumbItem>
+                <BreadcrumbSeparator className="hidden md:block" />
+                <BreadcrumbItem>
+                  <BreadcrumbPage>Data Fetching</BreadcrumbPage>
+                </BreadcrumbItem>
+              </BreadcrumbList>
+            </Breadcrumb>
+          </div>
+        </header>
+        <main>
+          <Outlet />
+        </main>
+      </SidebarInset>
     </SidebarProvider>
     </ThemeProvider>
   )
 }
+
